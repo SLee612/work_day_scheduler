04ff7c75dbaf5949fa114ef03cd056bcdddad44e
SLee612 <sarahlee612@gmail.com>
2021-03-18 15:14:49 -0500

    updated readme and a few tweaks

--- assets/README.md
+++ assets/README.md
@@ -1,2 +1,14 @@
 # work_day_scheduler
 Daily Planner Homework 3.17
+
+This is a work day planner.
+
+The user can type in events for each hour 9am-5pm and then save them by clicking the check button.
+
+After refreshing the page the events will remain where they were typed.
+
+The planner is color coded for past, present and future tasks.
+
+![Top](https://user-images.githubusercontent.com/79335372/111691346-3673d000-87fc-11eb-9adb-55f199af729a.png)
+
+![Bottom](https://user-images.githubusercontent.com/79335372/111691453-54d9cb80-87fc-11eb-9ada-c975caee6980.png)
--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,22 @@
-// Display today's day and date
+
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
-
 $(document).ready(function () {
-    // saveBtn click listener 
+    
     $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
+       
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        // Save text in local storage
         localStorage.setItem(time, text);
     })
    
     function timeTracker() {
-        //get current number of hours.
         var timeNow = moment().hour();
 
-        // loop over time blocks
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-            // To check the time and add the classes for background indicators
             if (blockTime < timeNow) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
@@ -41,7 +36,6 @@ $(document).ready(function () {
         })
     }
 
-    // Get item from local storage if any
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
