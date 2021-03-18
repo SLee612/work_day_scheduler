180bef53299e341d68f56355dcae685cc3ac7f14
SLee612 <sarahlee612@gmail.com>
2021-03-18 13:55:21 -0500

    Thursday

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,57 @@
-// var currentTime = Moment.currentTime()
+// Display today's day and date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
 $(document).ready(function () {
-    // var timeInputs = JSON.parse(localStorage.getItem('timeInputs')) || {};
-    var currentHour = moment().hours();
-    var todaysDate = moment().format("dddd, MMMM Do YYYY");
-    console.log(currentHour);
-    //Create Variable with the hours.
-    var timeInputs = [
-        { time: 9, input: "" },
-        { time: 10, input: "" },
-        { time: 11, input: "" },
-        { time: 12, input: "" },
-        { time: 1, input: "" },
-        { time: 2, input: "" },
-        { time: 3, input: "" },
-        { time: 4, input: "" },
-        { time: 5, input: "" },
-        
-    ]
-    //Show Today's date on the DOM
-    function printTime() {
-        $("#currentDay").text(todaysDate);
-    }
-    //Show Hours on the DOM
-    function printInputBlocks() {
-        for (let i = 0; i < timeInputs.length; i++) {
-            console.log(timeInputs[i].time, timeInputs[i].input);
-            var inputGroup = $('<div class="input-group mb-3">');
-            var inputGroupPrepend = $('<div class="input-group-prepend">');
-            var prependSpan = $('<span class="input-group-text">' + timeInputs[i].time + ':00' + '</span>');
-            inputGroupPrepend.append(prependSpan);
-            var inputEl = $('<input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" value="' + timeInputs[i].input + '">');
-            var inputGroupAppend = $('<div class="input-group-append">');
-            var appendSpan = $('<span data-time="" class="input-group-text"><button>Save</button></span>');
-            inputGroupAppend.append(appendSpan);
-            inputGroup.append(inputGroupPrepend).append(inputEl).append(inputGroupAppend);
-            $(".container").append(inputGroup);
-        }
-    }
-    function compareTime() {
-        var nowTime = parseInt(moment().format('HH'));
-        //Start from 9AM, till 5PM
-        for (time = 9; time >= 15; time++) {
-            var timeBlock = parseInt($("#" + time + "hr").attr("data-index"));
-            console.log(timeBlock);
-            if (timeBlock < nowTime) {
-                $("#" + time + "hr").addClass("past");
-            } else if (timeBlock == nowTime) {
-                $("#" + time + "hr").addClass("present");
-            } else if (timeBlock > nowTime) {
-                $("#" + time + "hr").addClass("future");
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+   
+    function timeTracker() {
+        //get current number of hours.
+        var timeNow = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
             }
-        }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
     }
-        compareTime();
-    printTime();
-    printInputBlocks();
-});
+
+    // Get item from local storage if any
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    timeTracker();
+})
